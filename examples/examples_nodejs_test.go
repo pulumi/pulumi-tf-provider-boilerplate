aef2259db7e81579c77d6c477f88f66f2c9e04eb
Josh Kodroff <josh@pulumi.com>
2021-11-04 19:07:45 -0400

    Fix text in test file.

--- examples/examples_nodejs_test.go
+++ examples/examples_nodejs_test.go
@@ -14,7 +14,7 @@ func getJSBaseOptions(t *testing.T) integration.ProgramTestOptions {
 	base := getBaseOptions()
 	baseJS := base.With(integration.ProgramTestOptions{
 		Dependencies: []string{
-			"@pulumi/sumologic",
+			"@pulumi/foo",
 		},
 	})
 
