e5402222b9d7011d83a9d0c333cba5bde24c3c82
Vincent Bernat <vincent@bernat.ch>
2022-04-27 01:49:09 +0200

    Use go:embed instead of go generate for schema (#56)
    This makes the provider go-gettable. This is supported since Go 1.16.
    Fix #54.

--- provider/cmd/pulumi-resource-xyz/main.go
+++ provider/cmd/pulumi-resource-xyz/main.go
@@ -12,15 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:generate go run ./generate.go
-
 package main
 
 import (
+	_ "embed"
+
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 	xyz "github.com/pulumi/pulumi-xyz/provider"
 	"github.com/pulumi/pulumi-xyz/provider/pkg/version"
 )
+
+//go:embed schema.json
+var pulumiSchema []byte
 
 func main() {
 	// Modify the path to point to the new provider
