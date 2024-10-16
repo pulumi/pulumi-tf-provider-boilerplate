4bbec17fbaba34527ceef3b90258920bb73454c7
Toni Kangas <kangasta@users.noreply.github.com>
2024-10-16 17:10:36 +0300

    Fix `PluginDownloadURL` example path

--- provider/resources.go
+++ provider/resources.go
@@ -124,7 +124,7 @@ func Provider() tfbridge.ProviderInfo {
 		LogoURL: "",
 		// PluginDownloadURL is an optional URL used to download the Provider
 		// for use in Pulumi programs
-		// e.g https://github.com/org/pulumi-provider-name/releases/
+		// e.g. https://github.com/org/pulumi-provider-name/releases/download/v${VERSION}/
 		PluginDownloadURL: "",
 		Description:       "A Pulumi package for creating and managing xyz cloud resources.",
 		// category/cloud tag helps with categorizing the package in the Pulumi Registry.
